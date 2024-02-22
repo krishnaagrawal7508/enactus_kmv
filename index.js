b0f625f24599ccfcc0be0aa1093850f46e99cf27
krishna agrawal <agrawalkrishna522@gmail.com>
2024-02-22 15:58:11 +0530

    learning vercel

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 import express from "express";
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const app = express();
-app.set('view engine', 'ejs');
+app.set("views", __dirname + "/views");
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "public"));
+
 const port = 9000;
 
 app.use(express.static("public"));
